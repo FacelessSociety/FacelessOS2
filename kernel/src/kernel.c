7ddc8e8c10f2345a4d415c54238c71929509c907
Ian-M-Moffett <spruiousvector@gmail.com>
2022-06-06 04:58:47 -0400

    refactor - changed the wording of a message.

--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -96,9 +96,9 @@ static void init(struct stivale2_struct* ss) {
     idt_install();
     log(KINFO "Interrupt Descriptor Table loaded.\n");
     pmm_init(ss);
-    log(KINFO "PMM all setup.\n");
+    log(KINFO "Finished setting up PMM.\n");
     vmm_init(ss);
-    log(KINFO "VMM all setup.\n");
+    log(KINFO "Finished setting up VMM.");
 }
 
 
